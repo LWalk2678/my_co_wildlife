8b060dc1c086de750b9936c7678a4f28f032ba09
Lance Walker <LWalk2678@msn.com>
2021-12-14 14:39:04 -0700

    working on postman error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #From postmand added this to allow useage
+  config.hosts << "localhost"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
