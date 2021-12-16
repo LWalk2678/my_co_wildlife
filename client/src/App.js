9d7c5272ae8daad3058237620725efe85b6b48af
Lance Walker <LWalk2678@msn.com>
2021-12-16 00:44:31 -0700

    updated Layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import Layout from './layouts/Layout';
 import { Switch, Route, useHistory } from 'react-router-dom';
 import Login from './screens/Login';
-import {loginUser, registerUser, verifyUser} from './services/auth'
+import {loginUser, registerUser, removeToken, verifyUser} from './services/auth'
 import Register from './screens/Register';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
 
   useEffect(() => {
     const handleVerify = async () => {
-      const userData = awaitverifyUser();
+      const userData = await verifyUser();
       setCurrentUser(userData)
     }
   })
@@ -29,9 +29,15 @@ function App() {
     //history.push('/')
   };
 
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  }
+
   return (
     <div className="App">
-      <Layout currentUser={currentUser}>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
         <Switch>
           <Route path='/login'>
             <Login handleLogin={handleLogin}/>
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom"
 
-export default function Layout(props) {
-  const { currentUser } = props;
-
+export default function Layout({ currentUser, handleLogout, children }) {
+  
   return (
     <div>
       <header>
         <h1>My Colorado Wildlife</h1>
         {currentUser ? (
           <div>
-            <p>{currentUser.username}</p>
-            <button>Logout</button>
+            <p>Welcome {currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
           </div>
         ) : (
           <Link to='/login'>Login/Register</Link>
         )}
       </header>
-      {props.children}
+      {children}
     </div>
   )
 }
